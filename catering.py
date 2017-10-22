3cd4e488b7b7e5b5eb8a50354bfa22570c93c14a
Jordan Grogan <jordangrogan11@gmail.com>
2017-10-22 14:50:13 -0400

    Updated models, added templates, and added functionality to catering.py

--- catering.py
+++ catering.py
@@ -6,10 +6,11 @@
 """
 
 from flask import Flask, request, session, url_for, redirect, render_template, flash
-from models import db, Customer, Staffer, Event
+from models import db, User, Event
 
 app = Flask(__name__)
 
+app.secret_key = "this is a terrible secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catering.db'
 db.init_app(app)
 
@@ -20,21 +21,154 @@ def initdb_command():
 	db.create_all()
 	print('Initialized the database.')
 
-@app.route('/login', methods=['GET', 'POST'])
+# by default, direct to login
+@app.route("/", methods=['GET', 'POST'])
+def default():
+	"""Default page."""
+
+	if "username" in session:
+		user = User.query.get(session['user_id'])
+
+		if request.method == 'POST' and session['role'] == 2:
+			# Add an Event
+			if not request.form['name']:
+				flash("Please enter an event name.")
+			elif not request.form['date']:
+				flash("Please enter an event date.")
+			elif Event.query.filter_by(date = request.form['date']).count() > 0:
+				flash("Sorry, that date is not available.")
+			else:
+				db.session.add(Event(name=request.form['name'], date=request.form['date'], customer_id=user.id))
+				db.session.commit()
+				flash("Your event has been added!")
+
+		if session['role'] == 0: # Owner
+			events = Event.query.all()
+			return render_template('default_owner.html', events=events)
+		elif session['role'] == 1: # Staff
+			yourevents = user.staffer_events
+			unfilledevents = Event.query.all() # need to work on some sort of filter
+				# filter that (1) not already signed up for (2) doesn't have all three spots filled
+			return render_template('default_staff.html', yourevents=yourevents, unfilledevents=unfilledevents)
+		elif session['role'] == 2: # Customer
+			yourevents = user.customer_events
+			return render_template('default_customer.html', yourevents=yourevents)
+
+	return redirect(url_for('login'))
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
 	"""Logs the user in."""
-	if g.user:
-		return redirect(url_for('timeline'))
-	error = None
-	if request.method == 'POST':
 
-		user = User.query.filter_by(username=request.form['username']).first()
-		if user is None:
-			error = 'Invalid username'
-		elif not check_password_hash(user.pw_hash, request.form['password']):
-			error = 'Invalid password'
+	# first check if the user is already logged in
+	if "username" in session:
+		flash("Already logged in!")
+		return redirect(url_for('default'))
+
+	# if not, and the incoming request is via POST try to log them in
+	elif request.method == 'POST':
+		if request.form['username'] == "owner" and request.form['password'] == "pass":
+			session['user_id'] = 0
+			session['username'] = "owner"
+			session['role'] = 0
+			return redirect(url_for('default'))
 		else:
-			flash('You were logged in')
-			session['user_id'] = user.user_id
-			return redirect(url_for('timeline'))
-	return render_template('login.html', error=error)
+			user = User.query.filter_by(username=request.form['username']).first()
+			if user is None:
+				flash("Invalid username, please try again.")
+			elif user.password != request.form['password']:
+				flash("Invalid password, please try again.")
+			else:
+				session['user_id'] = user.id
+				session['username'] = user.username
+				session['role'] = user.role
+				return redirect(url_for('default'))
+
+	return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+	"""Logs the user out."""
+
+	# if logged in, log out, otherwise offer to log in
+	if "username" in session:
+		session.clear()
+		flash("Successfully logged out!")
+	else:
+		flash("Not currently logged in!")
+
+	return redirect(url_for("login"))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	"""Registers the user."""
+
+	if "username" in session:
+		return redirect(url_for('default'))
+
+	if request.method == 'POST':
+		if not request.form['username']:
+			flash("Please enter a username.")
+		elif not request.form['password']:
+			flash("Plase enter a password.")
+		elif User.query.filter_by(username = request.form['username']).count() > 0:
+			flash("Sorry, that username is already taken.")
+		else:
+			db.session.add(User(username=request.form['username'], password=request.form['password'], role=2))
+			db.session.commit()
+			flash("You were successfully registered and can login now.")
+			return redirect(url_for('login'))
+	return render_template('register.html')
+
+@app.route('/cancel')
+def cancel():
+	"""Cancel an event."""
+
+	if request.args.get('event'):
+		if Event.query.get(request.args.get('event')).customer_id == session['user_id']:
+			ename = Event.query.get(request.args.get('event')).name
+			db.session.delete(Event.query.get(request.args.get('event')))
+			db.session.commit()
+			flash("{} has been cancelled.".format(ename))
+		else:
+			flash("You do not have permission to cancel that event.")
+	else:
+		flash("Error getting event.")
+
+	return redirect(url_for('default'))
+
+@app.route('/addstaff', methods=['GET', 'POST'])
+def addstaff():
+	"""Owner can add staff."""
+
+	if "username" not in session and session['role'] != 0:
+		flash("You do not have permission to access that page.")
+		return redirect(url_for('default'))
+	else:
+		if request.method == 'POST':
+			if not request.form['username']:
+				flash("Please enter a username.")
+			elif not request.form['password']:
+				flash("Plase enter a password.")
+			elif User.query.filter_by(username = request.form['username']).count() > 0:
+				flash("Sorry, that username is already taken.")
+			else:
+				db.session.add(User(username=request.form['username'], password=request.form['password'], role=1))
+				db.session.commit()
+				flash("That staffer was successfully added and can login now.")
+				return redirect(url_for('default'))
+		return render_template('addstaff.html')
+
+@app.route('/signup')
+def signup():
+	"""Staff can sign up for an event."""
+
+	if request.args.get('event') and session['role'] == 1:
+		event = Event.query.get(request.args.get('event'))
+		event.staffers.append(User.query.get(session['user_id']))
+		db.session.commit()
+		flash("You are now signed up for {}.".format(event.name))
+	else:
+		flash("Error getting event.")
+
+	return redirect(url_for('default'))
--- models.py
+++ models.py
@@ -2,48 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Customer(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(100), nullable=False, unique=True)
-	password = db.Column(db.String(100), nullable=False)
-
-	events = db.relationship('Event', backref='customer')
-
-	def __init__(self, username, password):
-		self.username = username
-		self.password = password
-
-	def __repr__(self):
-		return '<Customer {}>'.format(self.username)
-
 schedules = db.Table('schedules',
-	db.Column('staffer_id', db.Integer, db.ForeignKey('staffer.id')),
+	db.Column('staffer_id', db.Integer, db.ForeignKey('user.id')),
 	db.Column('event_id', db.Integer, db.ForeignKey('event.id'))
 )
 
-class Staffer(db.Model):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(24), nullable=False)
-	password = db.Column(db.String(64), nullable=False)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(120))
+	role = db.Column(db.Integer) # staffer = 1 / customer = 2
+	customer_events = db.relationship('Event', backref='customer')
+	staffer_events = db.relationship('Event', secondary=schedules, lazy=True, backref=db.backref('staffers', lazy="select"))
 
-	events = db.relationship('Event', secondary=schedules, backref=db.backref('staffers', lazy='dynamic'))
-
-	def __init__(self, username, email, pw_hash):
+	def __init__(self, username, password, role):
 		self.username = username
-		self.pw_hash = pw_hash
+		self.password = password
+		self.role = role
 
 	def __repr__(self):
-		return '<Staffer {}>'.format(self.username)
+		return '<User {}>'.format(self.username)
 
 class Event(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50))
 	date = db.Column(db.String(50))
-	customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+	customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-	def __init__(self, name, date):
+	def __init__(self, name, date, customer_id):
 		self.name = name
 		self.date = date
+		self.customer_id = customer_id
 
 	def __repr__(self):
-		return '<Event {} ({})>'.format(self.username, self.date)
+		return '<Event {} ({})>'.format(self.name, self.date)
